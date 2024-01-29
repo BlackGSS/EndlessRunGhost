ffa849880fc872a4f5b15b9cbf8b9da6bd8547e5
Neisum <nathan.pm96@gmail.com>
2024-01-29 12:28:52 +0100

    - Clean unused scripts

--- Assets/Scripts/ScriptableEvents/IScriptableEvents.cs
+++
@@ -1,9 +0,0 @@
-namespace Neisum.ScriptableEvents
-{
-    public interface IScriptableEvents<T>
-    {
-        void RaiseScriptableUpdatedEvent(T data);
-        void AddListener(IScriptableEventListener<T> listener);
-        void RemoveListener(IScriptableEventListener<T> listener);
-    }
-}
