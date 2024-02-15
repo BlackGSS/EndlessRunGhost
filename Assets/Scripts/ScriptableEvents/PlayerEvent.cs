89c7e8cbc5b1ea5f7d6162f5f077411de87cc182
Neisum <nathan.pm96@gmail.com>
2024-02-15 12:35:03 +0100

    * Event renamed

--- Assets/Scripts/ScriptableEvents/PlayerEvent.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerSpawnedEvent", menuName = "ScriptableEvents/PlayerEvent", order = 0)]
-public class PlayerEvent : BaseEvent<PlayerControl>
-{
-    public PlayerControl player;
-}
---
+++ Assets/Scripts/ScriptableEvents/PlayerSpawn.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSpawnEvent", menuName = "ScriptableEvents/PlayerSpawn", order = 0)]
+public class PlayerSpawn : BaseEvent<PlayerControl>
+{ }
