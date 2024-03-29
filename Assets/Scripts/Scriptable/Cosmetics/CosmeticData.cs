a0d810db8a8aff7ba88a6f7260a0e00d35a06e0d
Neisum <nathan.pm96@gmail.com>
2024-03-29 13:58:18 +0100

    + Pico and ramen fbx + Cometics icons in data

--- Assets/Scripts/Scriptable/Cosmetics/CosmeticData.cs
+++ Assets/Scripts/Scriptable/Cosmetics/CosmeticData.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Scriptables/CosmeticData")] [System.Serializable]
+[CreateAssetMenu(menuName = "Scriptables/CosmeticData")]
+[System.Serializable]
 public class CosmeticData : ScriptableObject
 {
     public int id;
     public Sprite image;
     public int price;
+    public bool renameOnValidate = false;
+
+    private void OnValidate()
+    {
+        if (renameOnValidate)
+        {
+            string thisFileNewName = id + "_" + this.name;
+            string assetPath = UnityEditor.AssetDatabase.GetAssetPath(this.GetInstanceID());
+            UnityEditor.AssetDatabase.RenameAsset(assetPath, thisFileNewName);
+            renameOnValidate = true;
+        }
+    }
 }
