48df539084535d6f9250e6603d063b491ee0cf19
Neisum <nathan.pm96@gmail.com>
2024-03-31 19:49:14 +0200

    + Shield anim

--- Assets/Scripts/ScriptableEvents/Listeners/EnableShieldCollectListener.cs
+++ Assets/Scripts/ScriptableEvents/Listeners/EnableShieldCollectListener.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Neisum.ScriptableEvents;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class EnableShieldCollectListener : ScriptableListener<bool, PowerUpCollectEvent, UnityEvent<bool>>
 {
+    [SerializeField] ShieldPowerUpData shieldData;
     [SerializeField] MeshRenderer shieldMesh;
 
     protected override void OnEnable()
     {
         base.OnEnable();
-        shieldMesh.enabled = false;
+        ActivateShield(false);
     }
 
     public void ActivateShield(bool activate)
     {
         shieldMesh.enabled = activate;
+        Sequence sequence = DOTween.Sequence();
+        shieldMesh.material.SetFloat("_Fill", activate ? 1 : 0);
+        sequence.Append(shieldMesh.material.DOFloat(activate ? 0 : 1, "_Fill", activate ? shieldData.duration * 2 : 0));
+        // sequence.OnComplete(() => ));
     }
 }
